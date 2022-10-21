e050b16f7dbe2202c349f75ba1a758a28779f908
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-10-21 10:15:17 +0600

    js code

---
+++ script.js
@@ -0,0 +1,182 @@
+/* 
+
+how to declare a variable in js 
+
+4 ways to  declare javascript variable 
+var , let , const , nothing 
+
+different between = , == , ===   ! ;
+
+
+3 type of variable in js 
+1. number ,
+2. string ,
+3. boolean 
+*/
+
+// var x = 10 
+// var y = 20 
+// var z = x + y 
+
+// let x = 10;
+// let y = 20;
+// let z = x + y;
+
+
+// const x = 10;
+// const y = 20;
+// const z = x / y;
+
+// console.log(z);
+
+
+// string ---
+
+// var javascript = 10;
+
+// var name = "javascript" ;
+
+// var x = "10";
+// var y = "20";
+// var z = x + y ;
+// console.log(z);
+
+
+// variable shuld be kanel case ;
+
+// var firstName = "hello";
+// var secondName = "javascript";
+
+// var result = firstName + secondName;
+
+// console.log(result);
+
+// number  or string addition 
+
+// check type typeof
+
+// var num = 10 ;
+// var num2 = "20";
+// var result = num + num2;
+
+
+// var person = {
+//     name : "javascript"
+// }
+// console.log(typeof person);
+
+// console.log(result);
+
+// true , false 
+
+// === type 
+
+// var num = 10 ;
+// var num2 = "10";
+
+// var result = num === num2 ;
+
+// console.log(result);
+
+//console.log ===> clg tab pree
+
+// bang sing ! means not equal
+
+
+
+// var num1 = 10;
+// var num2 = 10;
+
+//  var result = num1 !== num2;
+
+// console.log(result);
+
+// console.log(typeof "hello");
+
+// var hello = 10;
+
+// console.log(typeof hello);
+
+// var num1 = 40;
+// var num2 = 30;
+
+// var result = num1 >= num2 ;
+
+// console.log(result);
+
+
+
+// condition if  else if 
+
+// if(condition){
+
+// }
+
+
+// var x = 30;
+// var y = 20;
+
+// if(x > y){
+//     console.log('x is greater');
+// }
+
+
+
+
+
+// var x = 20 ;
+// var y = "20";
+
+// if (x === y){
+//     console.log('they are equal');
+// }
+//  else {
+//     console.log('they are not equal');
+// }
+
+
+// var x = 20 ;
+// var y = "20";
+
+// if (x === y){
+//     console.log('1st condition');
+// }
+
+// else if (x > y){
+//     console.log('2nd condition');
+// }
+
+// else if (x == y){
+//     console.log('3rd condition');
+// }
+
+//  else {
+//     console.log('they are not equal');
+// }
+
+// var num1 = 20;
+// var num2 = 30;
+// var num3 = 40;
+
+// if(num1 > num2){
+//     console.log("1st conditon true");
+// }
+
+// else if (num1 > num3){
+//     console.log("2nd conditon true");
+// }
+
+// else if (num2 < num1){
+//     console.log("3rd conditon is true");
+// }
+// else if (num2 > num3){
+//     console.log("4rd conditon is true");
+// }
+
+// else{
+//     console.log("no conditon are true");
+// }
+
+
+
+
